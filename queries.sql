8ebc6e3ca5670d41e6dbf2f22ca2c1d2dbb401f4
swatts204 <swatts204@cable.comcast.com>
2020-06-02 09:27:15 -0400

    Query updates

--- queries.sql
+++ queries.sql
@@ -1,14 +1,14 @@
 ## Part 1: Test it with SQL
-SELECT* FROM job;
+-- id INT, name VARCHAR, employer_id INT
 ## Part 2: Test it with SQL
 SELECT name From employer
-WHERE location"St.Louis";
+WHERE location like "%Louis";
 
 ## Part 3: Test it with SQL
 DROP TABLE job
 ## Part 4: Test it with SQL
-SELECT name, description
+SELECT*
 From skill
-INNER JOIN ON job_skills ON skill.id=job_skills.skills_id
+INNER JOIN job_skills ON skill.id=job_skills.skills_id
             WHERE jobs_id IS NOT NULL
-            ORDER BY ASC;
+            ORDER BY 'name';
